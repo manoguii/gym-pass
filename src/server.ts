471ae24639456c5eae83bc7e0af09a7370086652
guilherme david <guilhermedavidrk@icloud.com>
2023-03-09 19:05:13 -0300

    :zap: config: create structure project

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import { app } from './app'
+import { env } from './env'
+
+app
+  .listen({
+    port: env.PORT,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('🚀🚀🚀 http server running !')
+  })
