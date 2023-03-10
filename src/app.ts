c43adffca718ecb30fd224bf01338118f8819de7
guilherme david <guilhermedavidrk@icloud.com>
2023-03-09 22:20:38 -0300

    :zap: feat: create tables

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Gertrude Gomez',
+    email: 'gertrude@prisma.com',
+  },
+})
