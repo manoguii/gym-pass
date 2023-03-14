a4014b65859325a3928d69212366a586a27c6fbb
guilherme david <guilhermedavidrk@icloud.com>
2023-03-14 01:07:23 -0300

    :fire: feat: config database tests 2e2

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,42 @@
+import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
+
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable !')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    console.log('setup 🍏')
+    const schema = randomUUID()
+
+    const databaseUrl = generateDatabaseUrl(schema)
+
+    process.env.DATABASE_URL = databaseUrl
+
+    execSync('npx prisma migrate deploy')
 
     return {
       async teardown() {
-        console.log('teardown 🍎')
+        await prisma.$executeRawUnsafe(`
+          DROP SCHEMA IF EXISTS "${schema}" CASCADE
+        `)
+
+        await prisma.$disconnect()
       },
     }
   },
